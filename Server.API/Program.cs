06847f5179ff6d52e35c6bccec5e210cc6c0e43f
Zipora5572 <z0548545572@gmail.com>
2025-06-03 01:17:49 +0300

    fix

--- Server.API/Program.cs
+++ Server.API/Program.cs
@@ -108,18 +108,18 @@ builder.Services.AddScoped<IFolderService, FolderService>();
 builder.Services.AddScoped<IUserActivityService, UserActivityService>();
 builder.Services.AddAutoMapper(typeof(MappingProfile), typeof(MappingPostProfile));
 
-builder.Services.AddDbContext<IDataContext, DataContext>(
-    options => options.UseSqlServer(Environment.GetEnvironmentVariable("SQL_CONNECTION")));
 //builder.Services.AddDbContext<IDataContext, DataContext>(
-//    options => options.UseMySql(Environment.GetEnvironmentVariable("MYSQL_CONNECTION"), new MySqlServerVersion(new Version(8, 0, 34))));
+//    options => options.UseSqlServer(Environment.GetEnvironmentVariable("SQL_CONNECTION")));
+builder.Services.AddDbContext<IDataContext, DataContext>(
+    options => options.UseMySql(Environment.GetEnvironmentVariable("MYSQL_CONNECTION"), new MySqlServerVersion(new Version(8, 0, 34))));
 
 builder.Services.AddCors(opt => opt.AddPolicy("MyPolicy", policy =>
 {
     policy.WithOrigins(
         "http://localhost:5173",
-            "http://localhost:4200",
-            "https://examease-admin.onrender.com",
-            "https://examease-app.onrender.com"
+        "http://localhost:4200",
+        "https://examease-admin.onrender.com",
+        "https://examease-app.onrender.com"
         )
             .AllowAnyHeader()
           .AllowCredentials()
